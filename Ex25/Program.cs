2db0c427f73efd6dbeb90c65a3c837d1a73dc5de
MAX <maksimdemidovru@mail.ru>
2022-08-31 11:38:12 +0300

    add Ex25

--- Ex25/Program.cs
+++ Ex25/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите первое число");
+bool firstNumber = int.TryParse(Console.ReadLine(), out int x);
+
+Console.WriteLine("Введите второе число");
+bool sekondNumber= int.TryParse(Console.ReadLine(), out int y);
+
+if(firstNumber  != true || sekondNumber != true)
+{
+    Console.WriteLine("Числа введены не верно.");
+    return;
+}
+
+int result = GetCube(x);
+Console.WriteLine(result);
+int GetCube(int num)
+{
+    
+    for (int i = 1; i < y; i++)
+{
+    num = num*x;
+}
+return num;
+}
