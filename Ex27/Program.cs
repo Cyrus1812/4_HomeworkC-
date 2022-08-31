5c65d9170fdb26ebcd4d0407b00caf39d832263f
MAX <maksimdemidovru@mail.ru>
2022-08-31 13:18:26 +0300

    add Ex27

---
+++ Ex27/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Введите число");
+bool Number = int.TryParse(Console.ReadLine(), out int num);
+
+if(Number != true )
+{
+    Console.WriteLine("Число введено не верно.");
+    return;
+}
+
+int GetSum(int sum)
+{
+    int result = 0;
+    while(sum>0)
+    {
+        result += sum%10;
+        sum= sum/10;
+    }
+    return result;
+}
+ int res = GetSum(num);
+ Console.WriteLine($"Сумма чисел = {GetSum(num)}");
